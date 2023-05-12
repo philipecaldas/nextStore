0707789685d7621399dbbe9d430e3aaf3c185c91
philipecaldas <philipecaldasdev@gmail.com>
2023-05-12 14:03:50 -0400

    home done

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,21 @@
 import type { AppProps } from "next/app";
+import { globalStyles } from "../styles/global";
+import Image from "next/image";
+import logoImg from "../assets/nextStore_logo.svg";
+import { Container, Header } from "../styles/pages/app";
+
+globalStyles();
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Container>
+      <Header>
+        <div>
+          <Image src={logoImg} alt="" />
+          <h1>Next Store</h1>
+        </div>
+      </Header>
+      <Component {...pageProps} />
+    </Container>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,68 @@
-import { Inter } from "next/font/google";
+// import { Inter } from "next/font/google";
+// const inter = Inter({ subsets: ["latin"] });
+import Image from "next/image";
+import Link from "next/link";
+import { useKeenSlider } from "keen-slider/react";
+import "keen-slider/keen-slider.min.css";
+import { GetStaticProps } from "next";
+import Stripe from "stripe";
+import { stripe } from "../lib/stripe";
+import { HomeContainer, Product } from "../styles/pages/home";
 
-const inter = Inter({ subsets: ["latin"] });
-
-export default function Home() {
-  return <h1>Hello Next</h1>;
+interface HomeProps {
+  products: {
+    id: string;
+    name: string;
+    imageUrl: string;
+    price: number;
+  }[];
 }
+
+export default function Home({ products }: HomeProps) {
+  const [sliderRef] = useKeenSlider({ slides: { perView: 3, spacing: 48 } });
+
+  return (
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      {products?.map((product) => (
+        <Link href={`/product/${product.id}`} key={product.id}>
+          <Product className="keen-slider__slide">
+            {/* Add Blurhash to blur img before loaded */}
+            <Image src={product.imageUrl} width={580} height={580} alt="" />
+            <footer>
+              <strong>{product.name}</strong>
+              <span>{product.price}</span>
+            </footer>
+          </Product>
+        </Link>
+      ))}
+    </HomeContainer>
+  );
+}
+
+//getServerSideProps
+
+export const getStaticProps: GetStaticProps = async () => {
+  const res = await stripe.products.list({
+    expand: ["data.default_price"],
+  });
+
+  const products = res.data.map((product) => {
+    const usdCurrency = new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+    });
+    const price = product.default_price as Stripe.Price;
+
+    return {
+      id: product.id,
+      name: product.name,
+      imageUrl: product.images[0],
+      price: usdCurrency.format((price.unit_amount ?? 0) / 100),
+    };
+  });
+
+  return {
+    props: { products },
+    revalidate: 180, // 3 min
+  };
+};
